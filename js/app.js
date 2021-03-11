77b05e77a3949388a0ae447d877836de9cb0e622
mhendricks96 <mhendricks96@yahoo.com>
2021-03-10 18:51:36 -0800

    began working on event handlers

--- js/app.js
+++ js/app.js
@@ -1,2 +1,26 @@
 'use strict';
 // Global variables
+let caseValues = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+let firstCase = document.getElementById('first-case');
+let secondCase = document.getElementById('second-case');
+let thirdCase = document.getElementById('third-case');
+let fourthCase = document.getElementById('fourth-case');
+let fifthCase = document.getElementById('fifth-case');
+let sixthCase = document.getElementById('sixth-case');
+let seventhCase = document.getElementById('seventh-case');
+let eighthCase = document.getElementById('eighth-case');
+let ninthCase = document.getElementById('ninth-case');
+let tenthCase = document.getElementById('tenth-case');
+
+
+
+firstCase.addEventListener('click', pickFirstCase);
+secondCase.addEventListener('click', pickSecondCase);
+thirdCase.addEventListener('click', pickThirdCase);
+fourthCase.addEventListener('click', pickFourthCase);
+fifthCase.addEventListener('click', pickFifthCase);
+sixthCase.addEventListener('click', pickSixthCase);
+seventhCase.addEventListener('click', pickSeventhCase);
+eighthCase.addEventListener('click', pickEighthCase);
+ninthCase.addEventListener('click', pickNinthCase);
+tenthCase.addEventListener('click', pickTenthCase);
