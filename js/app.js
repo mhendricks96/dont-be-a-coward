0933f200024ca577c0782c7bf26fcf238c9320d9
mhendricks96 <mhendricks96@yahoo.com>
2021-03-10 22:17:34 -0800

    added all buttons and functions to give each one random value

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@
 // Global variables
 let caseValues = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 let gameScreen = document.getElementById('game-screen');
+let div = document.createElement('div');
+let casesLeft = document.getElementById('cases-left');
+let casesList = document.createElement('li');
+
+
 let firstCase = document.getElementById('first-case');
 let secondCase = document.getElementById('second-case');
 let thirdCase = document.getElementById('third-case');
@@ -16,7 +21,12 @@ let tenthCase = document.getElementById('tenth-case');
 function gameLoop() {
   let valuePicked = caseValues.splice(Math.floor(Math.random() * caseValues.length), 1);
   console.log(valuePicked);
-  console.log(caseValues);
+  //console.log(caseValues);
+  div.textContent = `$${valuePicked}!!`;
+  gameScreen.appendChild(div);
+
+  casesList.textContent = caseValues;
+  casesLeft.appendChild(casesList);
 }
 
 
@@ -32,6 +42,55 @@ function pickSecondCase() {
   gameLoop();
   secondCase.removeEventListener('click', pickSecondCase);
 }
+
+function pickThirdCase() {
+  console.log('third case picked');
+  gameLoop();
+  thirdCase.removeEventListener('click', pickThirdCase);
+}
+
+function pickFourthCase() {
+  console.log('fourth case picked');
+  gameLoop();
+  fourthCase.removeEventListener('click', pickFourthCase);
+}
+
+function pickFifthCase() {
+  console.log('fifth case picked');
+  gameLoop();
+  fifthCase.removeEventListener('click', pickFifthCase);
+}
+
+function pickSixthCase() {
+  console.log('sixth case picked');
+  gameLoop();
+  sixthCase.removeEventListener('click', pickSixthCase);
+}
+
+function pickSeventhCase() {
+  console.log('seventh case picked');
+  gameLoop();
+  seventhCase.removeEventListener('click', pickSeventhCase);
+}
+
+function pickEighthCase() {
+  console.log('fourth case picked');
+  gameLoop();
+  eighthCase.removeEventListener('click', pickEighthCase);
+}
+
+function pickNinthCase() {
+  console.log('ninth case picked');
+  gameLoop();
+  ninthCase.removeEventListener('click', pickNinthCase);
+}
+
+function pickTenthCase() {
+  console.log('fourth case picked');
+  gameLoop();
+  tenthCase.removeEventListener('click', pickTenthCase);
+}
+
 
 
 firstCase.addEventListener('click', pickFirstCase);
